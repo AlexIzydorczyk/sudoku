a3ee4c01fd68ffef6895daebfa044f0b4a781bac
Alex Izydorczyk <aleks.izydorczyk@gmail.com>
2015-04-25 10:54:50 -0400

    Clean up

--- Makefile
+++ Makefile
@@ -3,14 +3,13 @@ LDFLAGS=-lm -larmadillo
 OBJ=solver.o altproj.o
 
 all: $(OBJ)
-	$(CXX) main.cpp -o main $(OBJ) $(CXXFLAGS) $(LDFLAGS)
+	$(CXX) -std=c++11 -stdlib=libc++ main.cpp -o main $(OBJ) $(CXXFLAGS) $(LDFLAGS)
 
 debug: $(OBJ)
-	$(CXX) debug.cpp -o debug $(OBJ) $(CXXFLAGS) $(LDFLAGS)
+	$(CXX) altproj.cpp -o debug $(OBJ) $(CXXFLAGS) $(LDFLAGS)
 
 %.o: %.cpp
-	$(CXX) -c $< -o $@ $(CXXFLAGS)
+	$(CXX) -std=c++11 -stdlib=libc++ -c $< -o $@ $(CXXFLAGS)
 
 clean:
-	rm $(OBJS) main debug
-
+	rm $(OBJS) main *.o
--- main.cpp
+++ main.cpp
@@ -44,53 +44,14 @@ void unitTest(){
 
 
 int main(){
-    //srand(time(NULL));
+
     srand(345);
-
-
-    /*
-    int puzzle[][9] = { {0, 0, 0,   7, 4, 0,    0, 0, 0},
-                        {0, 0, 4,   3, 0, 0,    0, 0, 0},
-                        {0, 0, 0,   0, 6, 0,    0, 0, 0},
-
-                        {8, 0, 9,   1, 0, 0,    7, 3, 6},
-                        {0, 4, 7,   6, 9, 0,    2, 1, 0},
-                        {1, 0, 0,   8, 0, 0,    9, 0, 4},
-
-                        {0, 1, 0,   0, 0, 0,    0, 0, 7},
-                        {9, 0, 6,   0, 8, 0,    0, 0, 3},
-                        {2, 0, 0,   4, 0, 3,    0, 0, 0}};
-     */
-    /*
-    int puzzle[][9] = { {0,0,0,    0,0,0,     0,0,0},
-                        {0,0,0,    0,0,0,     0,0,0},
-                        {0,0,0,    0,0,0,     0,0,0},
-
-                        {0,0,0,    0,0,0,     0,0,0},
-                        {0,0,0,    0,0,0,     0,0,0},
-                        {0,0,0,    0,0,0,     0,0,0},
-
-                        {0,0,0,    0,0,0,     0,0,0},
-                        {0,0,0,    0,0,0,     0,0,0},
-                        {0,0,0,    0,0,0,     0,0,0}};
-     */
-
-    int puzzle[4][4] = {{1,0,0,0},
-                        {0,2,0,0},
-                        {0,0,3,0},
-                        {0,0,0,4}};
-    //Board board(4);
-    //board.setFromArray(puzzle);
-    //bool isSolved = solve(board,0,0);
-    //printPuzzle(board);
-
 
     unitTest();
 
     string user_entry;
 
     Board board = generatePuzzle(4,2);
-    // cout << "Welcome to Sudoku" << endl << endl;
 
     regex rgx("[0-9]{1,}");
     smatch match;
@@ -132,26 +93,12 @@ int main(){
 
     } while (!board.checkPuzzle());
 
-    // cout << checkPuzzle(board) << endl;
-    // cout << "here is the puzzle" << endl;
-    // cout << "===========================================" << endl << endl;
-
     board.printPuzzle();
     if (board.checkPuzzle()){
         cout << "The puzzle is solved!" << endl;
     } else {
         cout << "Could not solve puzzle" << endl;
     }
-    // cout << "===========================================" << endl << endl;
-
-    // cout << "===========================================" << endl << endl;
-    // printPuzzle(board);
-    // cout << "===========================================" << endl << endl;
-    // bool isSolved = solve(board,0,0);
-
-    // printPuzzle(board);
-
-
 
     return 0;
 }
