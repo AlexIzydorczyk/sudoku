c24327a069e28a7760dbc5aa6dca51d12de0177d
Alex Izydorczyk <aleks.izydorczyk@gmail.com>
2015-04-26 22:59:24 -0400

    beginning code cleanup

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 CXXFLAGS=-std=c++11 -stdlib=libc++ -Wall -O3
 LDFLAGS=-lm -larmadillo
-OBJ=solver.o altproj.o
+OBJ=solver.o altproj.o game.o tests.o
 
 all: $(OBJ)
 	$(CXX) main.cpp -o main $(OBJ) $(CXXFLAGS) $(LDFLAGS)
---
+++ game.hpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <regex>
+
+#include "solver.hpp"
+#include "altproj.hpp"
+
+#include <sstream>
+
+
+void playGame(int size, int nobs); //main function to run the user interactive game
+void playAgainPrompt(int size, int nobs); // helper function to prompt user to play again
+
